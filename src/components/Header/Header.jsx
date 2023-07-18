f0df632303012811eb732d4e24eec6b50595f403
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-07-18 10:23:23 +0200

    create component Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./Header.scss"
+import ankorText from '../../assets/icons/ankorTextWhite.svg';
+
+
+
+export default function Header() {
+	return (
+		<header className="header">
+			<div className="header__content">
+				<div className="header__logo">
+					<img src={ankorText} alt="#" />
+				</div>
+				<ul className="header__nav">
+					<li className="nav__item">про нас</li>
+					<li className="nav__item">програма</li>
+					<li className="nav__item">локація</li>
+					<li className="nav__item">фотогалерея</li>
+					<li className="nav__item">команда</li>
+				</ul>
+				<h2 className="header__contacts">контакти</h2>
+			</div>
+		</header>
+	)
+}
