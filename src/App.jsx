80831211911e13a35df8212bd8a439e90e908aa1
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-07-18 10:21:30 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import "./App.css"
+import "./App.scss"
+import About from "./components/About/About"
+import Philosophy from "./components/Philosophy/Philosophy"
+import StartScreen from "./components/StartScreen/StartScreen"
 
 export default function App() {
-	return <div className="App"></div>
+	return <div className="App">
+		<StartScreen />
+		<About />
+		<Philosophy />
+	</div>
 }
