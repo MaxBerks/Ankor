ec3f6d766ce932d98e48623cabbe4ea6f8d89f93
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-07-20 15:22:19 +0200

    add adaptive

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Gallery.scss";
 import LogoTitle from "../LogoTitle/LogoTitle";
+import photo1 from "../../assets/images/greyBack.jpg";
+import photo2 from "../../assets/images/registrationPhoto.jpg";
+import PhotoSlider from "../PhotoSlider/PhotoSlider";
 
 export default function Gallery() {
-  return (
-    <section className="gallery">
-      <LogoTitle title="фотогалерея" subtitle="спогади з усіх років табору" />
-    </section>
-  );
+	const items = [photo1, photo2, photo1, photo2, photo1, photo2];
+
+	return (
+		<section className="gallery">
+			<LogoTitle title="фотогалерея" subtitle="спогади з усіх років табору" />
+			<div className="gallery__container">
+				<PhotoSlider items={items} />
+				<PhotoSlider items={items} />
+			</div>
+		</section>
+	);
 }
--- src/components/PhotoSlider/PhotoSlider.jsx
+++ src/components/PhotoSlider/PhotoSlider.jsx
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PhotoSlider.scss";
 
-export default function PhotoSlider({
-  photoSrc1,
-  photoSrc2,
-  photoSrc3,
-  photoSrc4,
-}) {
-  return (
-    <div className="photoSlider">
-      <div className="photoSlider__container">
-        <div className="photoSlider__btn photoSlider__btn-left"></div>
-        <div className="photoSlider__btn photoSlider__btn-right"></div>
-        <div className="photoSlider__content">
-          <img className="photoSlider__img" src={photoSrc1} alt="#" />
-          <img className="photoSlider__img" src={photoSrc2} alt="#" />
-          <img className="photoSlider__img" src={photoSrc3} alt="#" />
-        </div>
-      </div>
-    </div>
-  );
+export default function PhotoSlider({ items }) {
+	// const [currentSlide, setCurrentSlide] = useState(0);
+
+	// const maxSlide = slides.length - 1;
+
+	// useEffect(() => {
+	// 	const slides = document.querySelectorAll(".photoSlider__slide");
+	// 	slides.forEach((slide, index) => {
+	// 		slide.style.transform = `translateX(${index * 100}%)`;
+	// 	});
+	// }, []);
+
+	// const onNextSlide = () => {
+	// 	if (currentSlide === maxSlide) {
+	// 		setCurrentSlide(0);
+	// 	} else {
+	// 		setCurrentSlide(currentSlide + 1);
+	// 	}
+
+	// 	slides.forEach((slide, index) => {
+	// 		slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+	// 	});
+	// };
+
+	// const onPrevSlide = () => {
+	// 	if (currentSlide === 0) {
+	// 		setCurrentSlide(maxSlide);
+	// 	} else {
+	// 		setCurrentSlide(currentSlide - 1);
+	// 	}
+
+	// 	slides.forEach((slide, index) => {
+	// 		slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+	// 	});
+	// };
+
+	return (
+		<div className="photoSlider">
+			<div className="photoSlider__container">
+				<div className="photoSlider__btn photoSlider__btn-prev"></div>
+				<div className="photoSlider__btn photoSlider__btn-next"></div>
+				<div className="photoSlider__content">
+					{items.map((item, index) => {
+						return (
+							<div
+								className="photoSlider__slide"
+								style={{ transform: `translateX(${index * 100}%)` }}
+								key={index}
+							>
+								<img className="photoSlider__img" src={item} alt="#" />
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
